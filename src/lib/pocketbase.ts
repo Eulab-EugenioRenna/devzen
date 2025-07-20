ab79ed2ad8521a44174e28a88ac3b2eaee8d1777
Eugenio Renna <rennaeugenio92@gmail.com>
2025-07-20 00:15:34 +0000

    la chiamata va a spaces ma le collection sono queste
    NEXT_PUBLIC_POCKET

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -3,6 +3,12 @@ import PocketBase from 'pocketbase';
 if (!process.env.NEXT_PUBLIC_POCKETBASE_URL) {
   throw new Error('POCKETBASE_URL is not set in the environment variables. Please add it to your .env file.');
 }
+if (!process.env.NEXT_PUBLIC_POCKETBASE_COLLECTION_USERS) throw new Error('NEXT_PUBLIC_POCKETBASE_COLLECTION_USERS is not set');
+if (!process.env.NEXT_PUBLIC_POCKETBASE_COLLECTION_BOOKMARKS) throw new Error('NEXT_PUBLIC_POCKETBASE_COLLECTION_BOOKMARKS is not set');
+if (!process.env.NEXT_PUBLIC_POCKETBASE_COLLECTION_SPACES) throw new Error('NEXT_PUBLIC_POCKETBASE_COLLECTION_SPACES is not set');
+if (!process.env.NEXT_PUBLIC_POCKETBASE_COLLECTION_MENU) throw new Error('NEXT_PUBLIC_POCKETBASE_COLLECTION_MENU is not set');
+if (!process.env.NEXT_PUBLIC_POCKETBASE_COLLECTION_TOOLS_AI) throw new Error('NEXT_PUBLIC_POCKETBASE_COLLECTION_TOOLS_AI is not set');
+
 
 export const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
 
@@ -10,9 +16,10 @@ export const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
 // server-side actions. It's particularly important in Next.js.
 pb.autoCancellation(false);
 
-export const usersCollectionName = 'devzen_users';
-export const bookmarksCollectionName = 'bookmarks';
-export const spacesCollectionName = 'spaces';
-export const menuCollectionName = 'menu';
-export const toolsAiCollectionName = 'tools_ai';
+export const usersCollectionName = process.env.NEXT_PUBLIC_POCKETBASE_COLLECTION_USERS;
+export const bookmarksCollectionName = process.env.NEXT_PUBLIC_POCKETBASE_COLLECTION_BOOKMARKS;
+export const spacesCollectionName = process.env.NEXT_PUBLIC_POCKETBASE_COLLECTION_SPACES;
+export const menuCollectionName = process.env.NEXT_PUBLIC_POCKETBASE_COLLECTION_MENU;
+export const toolsAiCollectionName = process.env.NEXT_PUBLIC_POCKETBASE_COLLECTION_TOOLS_AI;
+
 export const menuRecordId = 'vph860h5ys84561';
