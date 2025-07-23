172a82b8b199e6197777691bcba8ce784b020a4c
Eulab-EugenioRenna <eulabconsult@gmail.com>
2025-07-23 10:01:22 +0200

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,16 +5,17 @@ export const Icons = {
   logo: (props: SVGProps<SVGSVGElement>) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
       fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
       stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
       {...props}
     >
-      <path d="M4 4h8a4 4 0 0 1 4 4v12a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2v-4" />
-      <path d="M20 12V8a4 4 0 0 0-4-4H8" />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+      />
     </svg>
   ),
 };
